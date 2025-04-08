5c204671e2d1041c0f67f4fe900b6ad0941cc1e1
unknown <jorge-2806@hotmail.com>
2025-04-08 13:33:30 +0200

    Añadida la base de las dos secciones principales

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
-
+import "./css/app.css";
+import MainPage from './components/MainPage.jsx';
+import DataPage from './components/DataPage.jsx';
 function App() {
-
+  // State para la gestión de los datos
+  const [data, setData] = useState({cod:200});
   return (
-    <>
-      
-    </>
+      <div className='appContainer'>
+          <div className="shadow"></div>
+          {data["cod"] == 200 ? <DataPage data={data} />: <MainPage 
+            code={data.cod}/> 
+          }
+      </div>
   )
 }
 
---
+++ src/components/DataPage.jsx
@@ -0,0 +1,10 @@
+import "../css/DataPage.css";
+export default function DataPage(data){
+    return(
+        <div className="dataPageContainer">
+            <div className="mainDataCont">
+                <h1>test</h1>
+            </div>
+        </div>
+    )
+}
