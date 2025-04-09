6d390ede65a9900c06a63b6d84ba71fd6b5c456d
unknown <jorge-2806@hotmail.com>
2025-04-09 13:15:40 +0200

    Añadidos efectos climaticos

--- src/components/DataPage.jsx
+++ src/components/DataPage.jsx
@@ -1,9 +1,34 @@
 import "../css/DataPage.css";
+import "../conts.js";
 export default function DataPage(data){
+    const MESES = [
+        "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+        "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre",
+    ];
+    const DIAS = [
+        "Lunes", "Martes","Miercoles", "Jueves", "Viernes", "Sábado", "Domingo"
+    ]
+    const fecha = new Date();
+    var displayFecha = `${DIAS[fecha.getDay()]}, ${fecha.getDate()} ${MESES[fecha.getMonth()]} ${fecha.getFullYear()}`;
+    var displayYear = `${MESES[fecha.getMonth()]} ${fecha.getFullYear()}`
+    console.log(displayFecha)
     return(
         <div className="dataPageContainer">
             <div className="mainDataCont">
-                <h1>test</h1>
+                <div className="videCont">
+                    <video autoPlay loop muted playsInline>
+                        <source src="../../resources/overlays/rain.mp4" type="video/mp4" />
+                    </video>
+                    <div className="content">
+                        <div className="location">
+                            <h1>Barcelona</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="subCont">
+                <h1>{displayYear}</h1>
+                <h3>{displayFecha}</h3>
             </div>
         </div>
     )
---
+++ src/conts.js
@@ -0,0 +1,5 @@
+export const URL = {
+    APIURL:'https://api.openweathermap.org/data/2.5/forecast?appid=3a22a24418832306bd2c5cb98931c5a8&units=metric&lang=es&q=',
+    APIKEY:'3a22a24418832306bd2c5cb98931c5a8',
+    ICONURL:'https://openweathermap.org/img/wn/',
+}
